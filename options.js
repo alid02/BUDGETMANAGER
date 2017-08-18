dba60d8586a4f85680357a2afc9a54d30b926668
alid02 <aliddidarali02@gmail.com>
2017-08-18 13:12:32 +0100

    notification
    when  amount reach to  limit amount its give a notification

--- options.js
+++ options.js
@@ -4,26 +4,36 @@ $(function() {
 
     chrome.storage.sync.get("limit",function(budget){
         $("#limit").val(budget.limit);
-        console.log("asdd");
+
 
         //we store the limit on text on input text h2 element
     })
 
     $("#saveLimit").click(function(){
          //when we click the save button
-         console.log("ttttttt");
+
         var limit = $("#limit").val();
-        console.log(limit);       // limit value will save in var limit
+           // limit value will save in var limit
         if(limit){                            // if the limit is given
             chrome.storage.sync.set({"limit": limit}, function(){
                 close();                     // set limit and store in chrome storage and close tab
             });
-            console.log("zzzzzz");
+
         }
     });
 
     $("#resetTotal").click(function(){
              //when click the reset
-        chrome.storage.sync.set({"total": 0});
+        chrome.storage.sync.set({'total': 0}, function(){
+            var notifOptions = {            // we define notification options
+                        type: 'basic',                // type of notification we can change it in chrome developer tools
+                        iconUrl: 'icon48.png',          // icon appear for limit
+                        title: 'Total reset!',
+                        message: "Total has been reset to 0!"
+
+                    };
+                    chrome.notifications.create(notifOptions);
+                    //here we use chrome api tocreate the notification
+        });
     });      // total value will set to 0 and store in the chrome storage
 });
--- popup.js
+++ popup.js
@@ -10,7 +10,7 @@ $(function(){
 
     // we need to define an event when we click it should do something
     $('#spendAmount').click(function(){ //click the spend button
-        chrome.storage.sync.get('total',function(budget){
+        chrome.storage.sync.get(['total','limit'],function(budget){
         //chrome api to store the data to total when we enter
             var newTotal = 0;
             if (budget.total){
@@ -23,7 +23,21 @@ $(function(){
             }
             //now we want to store our new total to total
 
-            chrome.storage.sync.set({'total': newTotal});
+            chrome.storage.sync.set({'total': newTotal}, function(){
+                // we took a call back function check if the new total exisited the limit
+                if (amount && newTotal >= budget.limit){   //if the limit is equal or greater the limit
+                    var notifOptions = {            // we define notification options
+                        type : 'basic',                // type of notification we can change it in chrome developer tools
+                        iconUrl: 'icon48.png',          // icon appear for limit
+                        title: 'Limit reached!',
+                        message: "Uh oh! looks like you've reached your Limit!"
+
+                    };
+                    console.log('hello')
+                    chrome.notifications.create(notifOptions);
+                    //here we use chrome api to create the notification
+                }
+            });
 
             $('#total').text(newTotal);
             $('#amount').val(''); // to make our spend text clear
